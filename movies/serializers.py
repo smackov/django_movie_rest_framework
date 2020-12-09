9b43f49b75d54571fde2b3c2f38f6a563acf3929
smackov <smackovdinislam@gmail.com>
2020-12-09 11:43:24 +0500

    Get the nested notes out

--- movies/serializers.py
+++ movies/serializers.py
@@ -1,6 +1,20 @@
 from rest_framework import serializers
 
 from .models import Movie, Review
+    
+
+class FilterReviewListSerializer(serializers.ListSerializer):
+    """The filter of reviews. Only parents"""
+    def to_representation(self, data):
+        data = data.filter(parent=None)
+        return super().to_representation(data)
+        
+        
+class RecursiveSerializer(serializers.Serializer):
+    """Output children recursively"""
+    def to_representation(self, value):
+        serializer = self.parent.parent.__class__(value, context=self.context)
+        return serializer.data
         
 
 class ReviewCreateSerializer(serializers.ModelSerializer):
@@ -12,10 +26,13 @@ class ReviewCreateSerializer(serializers.ModelSerializer):
 
 class ReviewSerializer(serializers.ModelSerializer):
     "Show the review"
+    children = RecursiveSerializer(many=True)
+    
     class Meta:
+        list_serializer_class = FilterReviewListSerializer
         model = Review
-        fields = ('name', 'text', 'parent')
-
+        fields = ('name', 'text', 'children')
+    
 
 class MovieListSerializer(serializers.ModelSerializer):
     "List of the movies"
